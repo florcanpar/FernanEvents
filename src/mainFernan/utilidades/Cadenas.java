ae4c54889cf4b33e4a34adb12188f420c32b3744
Aga <aga.laza.03@gmail.com>
2026-01-07 19:53:23 +0100

    contraseña fuerte (esperemos q funcione)

--- src/mainFernan/utilidades/Cadenas.java
+++ src/mainFernan/utilidades/Cadenas.java
@@ -2,4 +2,48 @@ package mainFernan.utilidades;
 
 public class Cadenas {
 
+    public static boolean contraseniaSonIguales (String contrasenia1, String contrasenia2){
+        return contrasenia1.equals(contrasenia2);
+    }
+
+    public static boolean contraseniaFuerte(String contrasenia){
+        if (contrasenia.length() < 8) return false;
+        if (!tieneMayuscula(contrasenia)) return false;
+        if (!tieneMayuscula(contrasenia)) return false;
+        if (!tieneNumero(contrasenia)) return false;
+        if (!tieneSimbolo(contrasenia)) return false;
+
+        return true;
+    }
+
+    public static boolean tieneMinuscula(String contrasenia){
+        for (int i = 0; i < contrasenia.length(); i++)
+            if (contrasenia.charAt(i) >= 'a' && contrasenia.charAt(i) <= 'z')
+                return true;
+        return false;
+    }
+
+    public static boolean tieneMayuscula(String contrasenia){
+        for (int i = 0; i < contrasenia.length(); i++)
+            if (contrasenia.charAt(i) >= 'A' && contrasenia.charAt(i) <= 'Z')
+                return true;
+        return false;
+    }
+
+    public static boolean tieneNumero(String contrasenia){
+        for (int i = 0; i < contrasenia.length(); i++)
+            if (contrasenia.charAt(i) >= 1 && contrasenia.charAt(i) <= 9)
+                return true;
+        return false;
+    }
+
+    public static boolean tieneSimbolo(String contrasenia){
+        for (int i = 0; i < contrasenia.length(); i++)
+            if (contrasenia.charAt(i) == ',' || contrasenia.charAt(i) == '.' || contrasenia.charAt(i) == '@' ||
+                    contrasenia.charAt(i) == '-' || contrasenia.charAt(i) == '_' || contrasenia.charAt(i) == '*' ||
+                    contrasenia.charAt(i) == '+' || contrasenia.charAt(i) == '/')
+                return true;
+        return false;
+    }
+
 }
